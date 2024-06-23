d5760c0053aaf21cbd68b8cf1f4fbb456d40e8b3
Abhishek Mali <abhishekrockstar545@gmail.com>
2024-06-23 17:06:10 +0530

    springdoc setup for swagger documentation

---
+++ product-service/src/main/java/com/shopping/productservice/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.shopping.productservice.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI productServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Online Shopping Application - Product Service API's")
+                        .description("This is the REST API Documentation for Product Service")
+                        .version("v0.0.1")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can refer to the api-docs as json")
+                        .url("http://localhost:8080/api-docs"));
+    }
+}
