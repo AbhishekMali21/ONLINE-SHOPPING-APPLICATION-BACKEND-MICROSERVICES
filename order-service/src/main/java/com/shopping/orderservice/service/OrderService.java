aa12d40a4d967ea4d40af605c93ccaedacaece3c
Abhishek Mali <abhishekrockstar545@gmail.com>
2025-05-04 21:48:27 +0530

    feat: integrate Kafka for order placement events and update dependencies

--- order-service/src/main/java/com/shopping/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/shopping/orderservice/service/OrderService.java
@@ -1,12 +1,16 @@
 package com.shopping.orderservice.service;
 
 import com.shopping.orderservice.entities.Order;
+import com.shopping.orderservice.event.OrderPlacedEvent;
 import com.shopping.orderservice.exception.InventoryServiceUnavailableException;
+import com.shopping.orderservice.exception.ProductOutOfStockException;
 import com.shopping.orderservice.repository.OrderRepository;
 import com.shopping.orderservice.request.OrderRequest;
 import com.shopping.orderservice.response.OrderResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,14 +21,18 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
 
+    @Value("${spring.kafka.template.default-topic}")
+    private String kafkaTopic;
+
     private final OrderRepository orderRepository;
     private final InventoryClientService inventoryClientService;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         try {
             boolean isProductInStock = inventoryClientService.isInStock(orderRequest.skuCode(), orderRequest.quantity());
             if (!isProductInStock) {
-                throw new RuntimeException("Order Failed: Inventory is not in stock");
+                throw new ProductOutOfStockException("Order Failed: Inventory is not in stock");
             }
 
             Order order = Order.builder()
@@ -34,6 +42,13 @@ public class OrderService {
                     .quantity(orderRequest.quantity())
                     .build();
             orderRepository.save(order);
+
+            // send the message to the kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - sending orderPlacedEvent {} to kafka topic {}", orderPlacedEvent, kafkaTopic);
+            kafkaTemplate.send(kafkaTopic, orderPlacedEvent);
+            log.info("End - sent orderPlacedEvent {} to kafka topic {}", orderPlacedEvent, kafkaTopic);
+
             log.info("order created with order id is: {}", order.getId());
             return "Order Placed Successfully";
         } catch (InventoryServiceUnavailableException e) {
